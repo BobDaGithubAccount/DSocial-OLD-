63f42c53c13d48491f87ef9812f02ea1d35eae08
BobDaGithubAccount <62474433+BobDaGithubAccount@users.noreply.github.com>
2021-11-17 17:07:41 +0000

    Update DSocial.sol

--- DSocial.sol
+++ DSocial.sol
@@ -1,225 +1,221 @@
 pragma solidity 0.8.10;
 pragma abicoder v2;
-
 // SPDX-License-Identifier: Proprietary
-
-contract DChat {
+contract DSocial {
     
-    address public owner = 0xc479DA9d29D528670A916ab4Bc0c4a059a9619a8;
-    
-    bool closed = false;
-    string closedmessage = "DSocial is down for maintenance!";
-    string successmessage = "a";
-    
-    function transferOwner(address luckyOne) public {
-        if(owner == msg.sender) {
-            owner = luckyOne;
-        }
+    constructor() public {
+        //Initalise stuff where needed here
     }
     
+    //
+    
+    event OwnerChangedEvent(address oldone, address newone);
+    address owner = 0xc479DA9d29D528670A916ab4Bc0c4a059a9619a8;
     function getOwner() public view returns(address) {
         return owner;
     }
-    
-    function shutdownContract() public {
+    function setOwner(address target) public {
+        if(owner == msg.sender) {
+            owner = target;
+            emit OwnerChangedEvent(msg.sender, target);
+        }
+    }
+    event ContractStatusChange(bool isOffline);
+    bool isContractOffline = false;
+    function setContractOffline(bool newValue) public {
         if(msg.sender == owner) {
-            closed = true;
+            isContractOffline = newValue;
+            emit ContractStatusChange(newValue);
+        }
+    }
+    //Don't call from within contract
+    function getContractStatus() public view returns(bool) {
+        return isContractOffline;
+    }
+    
+    string[] BroadcastedMessages;
+    
+    event PublicBroadcastEvent();
+    
+    function sendBroadcast(string memory text) public {
+        if(msg.sender == owner) {
+            BroadcastedMessages.push(text);
+            emit PublicBroadcastEvent();   
         }
     }
     
-    function startContract() public {
+    function getBroadcasts() public view returns(string[] memory) {
+        return BroadcastedMessages;
+    }
+    
+    function clearBroadcasts() public {
         if(msg.sender == owner) {
-            closed = false;
+            delete BroadcastedMessages;
         }
     }
     
-    function isContractOffline() public view returns(bool) {
-        return closed;
-    }
-    
-    function setErrorMessage(string memory errormessage) public {
-        closedmessage = errormessage;
-    }
-    
-    function getErrorMessage() public view returns(string memory) {
-        return closedmessage;
-    }
     //
-    //
-    struct GlobalUser {
-        string name;
-        string pfpLocation;
-        string description;
-        address Address;
-    }
+        event ProfileUpdateEvent(address target);
+    
+        struct GlobalUser {
+            string name;
+            string description;
+            string pfplocation;
+            address[] friends;
+            bool isBot;
+        }
+        
+        mapping(address => GlobalUser) GlobalUsers;
+        mapping(address => bool) ListOfUsers;
+        
+        function getGlobalUser(address target) public view returns(GlobalUser memory) {
+            return GlobalUsers[target];
+        }
+        
+        function updateGlobalUser(string memory name, string memory description, string memory pfplocation, bool isBot) public {
+            GlobalUser memory user = GlobalUsers[msg.sender];
+            GlobalUser memory gu = GlobalUser(name, description, pfplocation, user.friends, isBot);
+            GlobalUsers[msg.sender] = gu;
+            ListOfUsers[msg.sender] = true;
+            emit ProfileUpdateEvent(msg.sender);
+        }
+        
+        function addFriend(address target) public {
+            GlobalUsers[msg.sender].friends.push(target);
+        }
+        
+        function deleteFriend(address target) public {
+            address[] memory friendsList = GlobalUsers[msg.sender].friends;
+            for(uint i=0; i<friendsList.length; i++) {
+                if(friendsList[i] == target) {
+                    delete GlobalUsers[msg.sender].friends[i];
+                    break;
+                }
+            }
+        }
 
-    event ProfileUpdate(address indexed updated);
-    
-    mapping(address => GlobalUser) GlobalUsers;
-    
-    function getGlobalUser() public view returns(GlobalUser memory) {
-        return GlobalUsers[msg.sender];
-    }
-    function setGlobalUser(string memory name, string memory pfpLocation, string memory description, address Address) public {
-        GlobalUser memory user = GlobalUser(name, pfpLocation, description, Address);
-        GlobalUsers[msg.sender] = user;
-        emit ProfileUpdate(Address);
-    }
     //
-    //
-    struct DMail {
-        string msg;
+    
+    struct EMAIL {
+        string text;
         address sender;
     }
     
-    mapping(address => DMail[]) GlobalInbox;
-    mapping(address => DMail[]) SentItems;
+    mapping(address => EMAIL[]) GlobalInbox;
+    mapping(address => EMAIL[]) GlobalSentItems;
     
-    event SendEmail(address indexed _from, address indexed _to);
+    event EMailSentEvent(address sender, address target);
     
-    function getInbox() public view returns (DMail[] memory) {
+    function getInbox() public view returns(EMAIL[] memory) {
         return GlobalInbox[msg.sender];
     }
     
-    function sendEmail(address target, string memory text) public returns(string memory) {
-        if(isContractOffline() == false) {
-            DMail memory email = DMail(text, msg.sender);
+    function getSentItems() public view returns(EMAIL[] memory) {
+        return GlobalSentItems[msg.sender];
+    }
+    
+    function sendEmail(string memory text, address target) public returns(bool) {
+        if(isContractOffline==false) {
+            EMAIL memory email = EMAIL(text, msg.sender);
+            GlobalSentItems[msg.sender].push(email);
             GlobalInbox[target].push(email);
-            SentItems[msg.sender].push(email);
-            emit SendEmail(msg.sender, target);   
-            return successmessage;
+            emit EMailSentEvent(msg.sender, target);
+            return true;
         }
         else {
-            return closedmessage;
+            return false;
         }
     }
     
-    function getSentItems() public view returns (DMail[] memory) {
-        return SentItems[msg.sender];
+    function clearInbox() public {
+        delete GlobalInbox[msg.sender];
+    }
+    function clearSentItems() public {
+        delete GlobalSentItems[msg.sender];
     }
     
     //
-    //
-    event DMCreateEvent(address[] members);
-    event DirectMessageSendEvent(address sender, address target);
-    
+
     struct DirectMessage {
         string text;
         string time;
         address sender;
     }
-    
-    struct DM {
+
+    struct DmChat {
         string name;
         DirectMessage[] messages;
         address[] members;
     }
     
-    struct DMs {
-        DM[] dms;
-    }
+    uint256 DmChatCounter = 0;
     
-    uint256 dmCounter;
+    mapping(uint256=>DmChat) GlobalChats;
+    mapping(address=>uint256[]) GlobalUserChats;
     
-    mapping(address => DMs) GlobalDirectMessages;
+    event DmChatCreateEvent(uint256 id, address[] members);
     
-    DirectMessage[] messages_;
-    function createDM(string memory name, address target, string memory time) public returns(string memory){
-        delete messages_;
-        if(isContractOffline() == false) {
-            dmCounter = dmCounter + 1;
-            address[] memory members;
-            members[0] = msg.sender;
-            members[1] = target;
-            DirectMessage memory directmessage = DirectMessage("Beginning of chat thread!", time, getOwner());
-            messages_.push(directmessage);
-            DM memory dm = DM(name, messages_, members);
-            GlobalDirectMessages[msg.sender].dms.push(dm);
-            GlobalDirectMessages[target].dms.push(dm);
-            emit DMCreateEvent(members);
-            return successmessage;
-        }
-        else {
-           return closedmessage;
-        }
-    }
-    
-    function getDirectMessageContents(uint256 id) public view returns(DirectMessage[] memory) {
-        return GlobalDirectMessages[msg.sender].dms[id].messages;
-    }
-    
-    function getDMs() public view returns(DM[] memory) {
-        return GlobalDirectMessages[msg.sender].dms;
-    }
-    
-    function sendDirectMessage(string memory text, string memory time, address target) public returns(string memory) {
-        if(isContractOffline() == false) {
-            DirectMessage memory message = DirectMessage(text, time, msg.sender);
-            
-            DM[] memory senderDMs = GlobalDirectMessages[msg.sender].dms;
-            uint256 senderID = 0;
-            uint256 senderLength = senderDMs.length;
-            bool senderDmExists = false;
-            DM memory senderDM;
-            for(uint256 i = 0; i < senderLength; i++) {
-                if(senderDMs[i].members[0] == msg.sender) {
-                    senderDmExists = true;
-                    senderDM = senderDMs[i];
-                    senderID = i;
-                }
-                else if(senderDMs[i].members[1] == msg.sender) {
-                    senderDmExists = true;
-                    senderDM = senderDMs[i];
-                    senderID = i;
-                }
-                else {
-                    senderDmExists = false;
-                }
-            } 
-            
-            DM[] memory targetDMs = GlobalDirectMessages[target].dms;
-            uint256 targetID = 0;
-            uint256 targetLength = senderDMs.length;
-            bool targetDmExists = false;
-            DM memory targetDM;
-            for(uint256 i = 0; i < targetLength; i++) {
-                if(targetDMs[i].members[0] == target) {
-                    targetDmExists = true;
-                    targetDM = senderDMs[i];
-                    targetID = i;
-                }
-                else if(senderDMs[i].members[1] == target) {
-                    targetDmExists = true;
-                    targetDM = senderDMs[i];
-                    targetID = i;
-                }
-                else {
-                    targetDmExists = false;
-                }
-            } 
-            
-            if(senderDmExists == true) {
-                if(targetDmExists == true) {
-                    DirectMessage[] storage senderMessages = GlobalDirectMessages[msg.sender].dms[senderID].messages;
-                    senderMessages.push(message);
-                    GlobalDirectMessages[msg.sender].dms[senderID].messages = senderMessages;
-                    DM storage targetDm = GlobalDirectMessages[target].dms[targetID]; //TODO fix stack too deep error
-                    DirectMessage[] storage targetMessages = targetDm.messages;
-                    targetMessages.push(message);
-                    GlobalDirectMessages[target].dms[targetID].messages = targetMessages;
-                    emit DirectMessageSendEvent(msg.sender, target);
-                    return successmessage;       
-                }
-                else {
-                    return "ERROR";
+    function createDmGroup(string memory name, address target) public {
+        if(isContractOffline==false) {
+            address[] memory targetFriendsList = GlobalUsers[target].friends;
+            bool isFriend = false;
+            for(uint i = 0; i < targetFriendsList.length; i++) {
+                if(targetFriendsList[i]==msg.sender) {
+                    isFriend = true;
+                    break;
                 }
             }
-            else {
-                return "ERROR";
-            }
-        }
-        else {
-            return closedmessage;
+            if(isFriend == true) {
+                uint256 id = DmChatCounter+1;
+                DmChatCounter = id;
+                address[] memory members;
+                DirectMessage[] memory messages;
+                members[0] = msg.sender;
+                members[1] = target;
+                DmChat memory chat = DmChat(name, messages, members);
+                GlobalChats[id] = chat;
+                GlobalUserChats[msg.sender].push(id);
+                GlobalUserChats[target].push(id);
+                emit DmChatCreateEvent(id, members);
+            }   
         }
     }
+    
+    event DmChatDeleteEvent(uint256 id, address[] members);
+    
+    function leaveDmGroup(uint256 id) public {
+        if(isContractOffline==false) {
+            address[] memory members = GlobalChats[id].members;
+            address one = members[0];
+            address two = members[1];
+            if(one==msg.sender||two==msg.sender) {
+                for(uint i=0;i<GlobalUserChats[one].length;i++) {
+                    if(GlobalUserChats[one][i]==id) {
+                        delete GlobalUserChats[one][i];
+                    }
+                }
+                for(uint i=0;i<GlobalUserChats[two].length;i++) {
+                    if(GlobalUserChats[two][i]==id) {
+                        delete GlobalUserChats[two][i];
+                    }
+                }
+                delete GlobalChats[id];
+                emit DmChatDeleteEvent(id,members);
+            }
+        }
+    }
+    
+    function getDms() public view returns(uint256[] memory) {
+        return GlobalUserChats[msg.sender];
+    }
+    
+    function getDm(uint256 id) public view returns(DmChat memory) {
+        address[] memory members = GlobalChats[id].members;
+        address one = members[0];
+        address two = members[1];
+        if(one==msg.sender||two==msg.sender) {
+            return GlobalChats[id];
+        }
+    }
+    
 }
