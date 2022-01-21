be0c23db6dff1843202e716bf5ac3e146f5995d4
BobDaGithubAccount <62474433+BobDaGithubAccount@users.noreply.github.com>
2022-01-21 16:55:25 +0000

    rewrote it lol

--- FileContract.sol
+++ FileContract.sol
@@ -1,40 +1,77 @@
-pragma solidity 0.8.11;
-pragma abicoder v2;
+pragma solidity 0.8.10;
 // SPDX-License-Identifier: Proprietary
 contract FileContract {
+    
     struct File {
         string name;
-        string data;
-        address owner;
         bool restrictAccess;
         address[] allowedToSee;
+        bool isURL;
+        string url;
+        string data;
+        address owner;
     }
 
-    mapping(address => uint256[]) GlobalUserFileStorage;
-    mapping(uint256 => File) GlobalFileStorage;
+    mapping(address=>uint256[]) GlobalFileUsers;
+    mapping(uint256=>File) GlobalFileStorage;
 
     uint256 fileCounter = 0;
 
-    function getFiles() public view returns(uint256[] memory) {
-        return GlobalUserFileStorage[msg.sender];
+    event FileUploadEvent(uint256 id, address owner);
+    function uploadFile(string memory name, bool restrictAccess, address[] memory allowedToSee, bool isURL, 
+        string memory url, string memory data) public {
+            fileCounter++;
+            File memory file = File(name, restrictAccess, allowedToSee, isURL, url, data, msg.sender);
+            GlobalFileStorage[fileCounter] = file;
+            GlobalFileUsers[msg.sender].push(fileCounter);
+            emit FileUploadEvent(fileCounter, msg.sender);
     }
 
-    event FileUploadEvent(uint256 id);
-    event FileUpdateEvent(uint256 id);
-
-    function uploadFile(string memory name, string memory data, bool restrictAccess, address[] memory allowedToSee) public {
-        File memory file = File(name, data, msg.sender, restrictAccess, allowedToSee);
-        fileCounter = fileCounter + 1;
-        GlobalFileStorage[fileCounter] = file;
-        GlobalUserFileStorage[msg.sender].push(fileCounter);
-        emit FileUploadEvent(fileCounter);
-    }
-
-    function updateFile(uint256 id, string memory name, string memory data, bool restrictAccess, address[] memory allowedToSee) public {
-        if(GlobalFileStorage[id].owner == msg.sender) {
-            File memory file = File(name, data, msg.sender, restrictAccess, allowedToSee);
+    event FileUpdateEvent(uint256 id, address owner);
+    function updateFile(uint256 id, string memory name, bool restrictAccess, address[] memory allowedToSee, bool isURL, 
+        string memory url, string memory data) public {
+            require(GlobalFileStorage[id].owner==msg.sender,"You don't own this file!");
+            File memory file = File(name, restrictAccess, allowedToSee, isURL, url, data, msg.sender);
             GlobalFileStorage[id] = file;
-            emit FileUpdateEvent(id);
+            emit FileUpdateEvent(id, msg.sender);
+    }
+
+    function allowPublicAccess(uint256 id, bool value) public {
+        require(GlobalFileStorage[id].owner==msg.sender,"You don't own this file!");
+        GlobalFileStorage[id].restrictAccess = value;
+    }
+
+    function addPersonToWhitelist(uint256 id, address target) public {
+        require(GlobalFileStorage[id].owner==msg.sender,"You don't own this file!");
+        GlobalFileStorage[id].allowedToSee.push(target);
+    }
+
+    function removePersonFromWhitelist(uint256 id, address target) public {
+        require(GlobalFileStorage[id].owner==msg.sender,"You don't own this file!");
+        for(uint i = 0; i < GlobalFileStorage[id].allowedToSee.length; i++) {
+            if(GlobalFileStorage[id].allowedToSee[i] == target) {
+                delete GlobalFileStorage[id].allowedToSee[i];
+                break;
+            }
         }
     }
+
+    function isWhitelisted(uint256 id, address target) public view returns(bool) {
+        for(uint i = 0; i < GlobalFileStorage[id].allowedToSee.length; i++) {
+            if(GlobalFileStorage[id].allowedToSee[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getFiles() public view returns(uint256[] memory) {
+        return GlobalFileUsers[msg.sender];
+    }
+
+    function getFile(uint256 id) public view returns(File memory) {
+        require(GlobalFileStorage[id].owner==msg.sender||isWhitelisted(id,msg.sender),"You don't have the permission to see this file!");
+        return GlobalFileStorage[id];
+    }
+
 }
