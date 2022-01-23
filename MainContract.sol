79b8aa299388d87f46111677d037b9c08bbd71c8
BobDaGithubAccount <62474433+BobDaGithubAccount@users.noreply.github.com>
2022-01-23 11:36:38 +0000

    Update MainContract.sol

--- MainContract.sol
+++ MainContract.sol
@@ -1,68 +1,71 @@
-pragma solidity 0.8.11;
-pragma abicoder v2;
+pragma solidity 0.8.10;
 // SPDX-License-Identifier: Proprietary
 contract MainContract {
     
-    event OwnerChangedEvent(address oldone, address newone);
-
-    address owner = 0xc479DA9d29D528670A916ab4Bc0c4a059a9619a8;
-    function getOwner() public view returns(address) {
-        return owner;
+    address public owner;
+    constructor() {
+        owner = msg.sender;
     }
 
-    function setOwner(address target) public {
-        if(owner == msg.sender) {
-            owner = target;
-            emit OwnerChangedEvent(msg.sender, target);
-        }
+    event OwnershipChangeEvent(address owner);
+    function transferOwnership(address target) public {
+        require(msg.sender==owner,"You aren't the owner of DSocial!");
+        owner = target;
+        emit OwnershipChangeEvent(target);
     }
-    
+
     string[] broadcasts;
+
+    event BroadcastEvent();
+    function sendBroadcast(string memory text) public {
+        require(msg.sender==owner,"You aren't the owner of DSocial!");
+        broadcasts.push(text);
+        emit BroadcastEvent();
+    }
 
     function getBroadcasts() public view returns(string[] memory) {
         return broadcasts;
     }
 
-    event BroadcastEvent();
-
-    function sendBroadcast(string memory text) public {
-        if(msg.sender == owner) {
-            broadcasts.push(text);
-            emit BroadcastEvent();
-        }
-    }
-
-    function clearBroadcasts() public {
-        if(msg.sender == owner) {
-            delete broadcasts;
-            emit BroadcastEvent();
-        }
-    }
-
-    mapping(string => address) contracts;
-
+    mapping(string=>address) contracts;
     string[] contractNames;
 
-    function getContracts() public view returns(string[] memory) {
-        return contractNames;
+    event ContractEvent(address target);
+
+    function pushContract(string memory name, address target) public {
+        require(msg.sender==owner,"You aren't the owner of DSocial!");
+        contracts[name] = target;
+        contractNames.push(name);
+        emit ContractEvent(target);
     }
+
+    function deleteContract(string memory name) public {
+        require(msg.sender==owner,"You aren't the owner of DSocial!");
+        for(uint i = 0; i < contractNames.length; i++) {
+            if(stringsEquals(contractNames[i],name)) {
+                delete contractNames[i];
+                delete contracts[name];
+                break;
+            }
+        }
+    }
+
+    function stringsEquals(string memory s1, string memory s2) private pure returns (bool) {
+    bytes memory b1 = bytes(s1);
+    bytes memory b2 = bytes(s2);
+    uint256 l1 = b1.length;
+    if (l1 != b2.length) return false;
+    for (uint256 i=0; i<l1; i++) {
+        if (b1[i] != b2[i]) return false;
+    }
+    return true;
+    }   
 
     function getContract(string memory name) public view returns(address) {
         return contracts[name];
     }
 
-    function deleteContract(string memory name) public returns(bool) {
-       if(msg.sender == owner) {
-           delete contracts[name];
-           return true;
-       }
-       return false;
-    }
-
-    function setContract(string memory name, address address_) public returns(bool) {
-        if(msg.sender == owner) {
-            contracts[name] = address_;
-        }
-        return false;    
+    function getContractNames() public view returns(string[] memory) {
+        return contractNames;
     }
 }
